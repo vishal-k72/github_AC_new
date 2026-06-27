f8f2b91a505efbaf38079c12e871e0368060d9bc
vishal-k72 <vkar3212@gmail.com>
2026-06-27 23:18:14 +0530

    add script and style file

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello Vishal Git Hub");
+let body = document.body;
+
+body.style.visibility = 'collapse';
+let navBar = document.querySelector('nav');
+
+setTimeout(() => {
+    body.style.visibility = 'visible';
+}, 400);
